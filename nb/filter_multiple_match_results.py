e347b71358b2b6aafb54abb0fbe557bd8595f666
Clayton Mork <mork.clayton3@gmail.com>
2019-10-07 09:03:33 +0900

    Started working on implementing functionality to sort match results by concept labels.

--- nb/filter_multiple_match_results.py
+++ nb/filter_multiple_match_results.py
@@ -11,17 +11,25 @@ matching_results_dict = load_word_matches(word_matches_save_path)
 
 multiple_match_count = 0
 
-# jap_vocab: JapaneseVocab, concept_labels: ConceptLabels, vocab_entry: VocabularyEntry
 def get_match_data(word: WordResult) -> (JapaneseVocab, ConceptLabels, VocabularyEntry):
     return word.jap_vocab, word.concept_labels, word.vocab_entry
 
 from src.lib.history_parsing.word_match_filter import WordMatchFilter
-matching_results_dict = WordMatchFilter.filter_by_match_count(matching_results_dict, target=1, ineq='>')
-matching_results_dict = WordMatchFilter.filter_by_jlpt_level(matching_results_dict, target=5, ineq='<=')
-matching_results_dict = WordMatchFilter.filter_by_common_words(matching_results_dict, target=True)
+# matching_results_dict = WordMatchFilter.filter_by_jlpt_level(matching_results_dict, target=5, ineq='<=')
+# matching_results_dict = WordMatchFilter.filter_by_common_words(matching_results_dict, target=False)
+# matching_results_dict = WordMatchFilter.filter_by_match_count(matching_results_dict, target=1, ineq='==')
 
-logger.cyan(f"len(matching_results_dict): {len(matching_results_dict)}")
-for i, key, matching_results in zip(range(len(matching_results_dict)), matching_results_dict.keys(), matching_results_dict.values()):
+common_matching_results_dict = WordMatchFilter.filter_by_common_words(matching_results_dict, target=True)
+common_matching_results_dict = WordMatchFilter.filter_by_match_count(common_matching_results_dict, target=1, ineq='==')
+uncommon_matching_results_dict = WordMatchFilter.filter_by_common_words(matching_results_dict, target=False)
+uncommon_matching_results_dict = WordMatchFilter.filter_by_match_count(uncommon_matching_results_dict, target=1, ineq='==')
+
+results = {}
+results.update(common_matching_results_dict)
+results.update(uncommon_matching_results_dict)
+
+logger.cyan(f"len(results): {len(results)}")
+for i, key, matching_results in zip(range(len(results)), results.keys(), results.values()):
     logger.yellow(f"============{i}: {key}============")
     for i, word in zip(range(len(matching_results)), matching_results):
         if i > 0:
--- src/lib/history_parsing/word_match_filter.py
+++ src/lib/history_parsing/word_match_filter.py
@@ -52,9 +52,11 @@ class WordMatchFilter:
             relevant_words = []
             for word in matching_results:
                 jap_vocab, concept_labels, vocab_entry = get_match_data(word)
-                if concept_labels is None:
+                if concept_labels is None and target == True:
                     continue
-                if concept_labels.is_common_word == target:
+                elif concept_labels is None and target == False:
+                    relevant_words.append(word)
+                elif concept_labels.is_common_word == target:
                     relevant_words.append(word)
             if len(relevant_words) > 0:
                 result[key] = relevant_words
@@ -123,3 +125,35 @@ class WordMatchFilter:
             if len(relevant_words) > 0:
                 result[key] = relevant_words
         return result
+
+class WordMatchSorter:
+    @classmethod
+    def sort_by_common_words(self, matching_results_dict: dict, mode: str='common_first'):
+        """
+        mode options: 'common_first', 'uncommon_first'
+        """
+        result = {}
+        common_results = WordMatchFilter.filter_by_common_words(matching_results_dict, target=True)
+        uncommon_results = WordMatchFilter.filter_by_common_words(matching_results_dict, target=False)
+        if mode == 'common_first':
+            result.update(common_results)
+            result.update(uncommon_results)
+        elif mode == 'uncommon_first':
+            result.update(uncommon_results)
+            result.update(common_results)
+        else:
+            logger.error(f"Invalid mode: {mode}")
+            raise Exception
+        return result
+
+    def sort_by_jlpt_level(self, matching_results_dict: dict, mode: str='ascending'):
+        """
+        mode options: 'ascending', 'descending'
+        """
+        pass
+
+    def sort_by_wanikani_level(self, matching_results_dict: dict, mode: str='ascending'):
+        """
+        mode options: 'ascending', 'descending'
+        """
+        pass
